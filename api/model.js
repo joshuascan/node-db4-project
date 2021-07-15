4f1598da6a72384532c12a2fef848ba07a809009
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-15 16:12:17 -0700

    WIP

--- api/model.js
+++ api/model.js
@@ -5,9 +5,39 @@ async function getRecipeById(recipe_id) {
     .leftJoin("steps as s", "r.recipe_id", "s.recipe_id")
     .leftJoin("step_ingredients as st", "s.step_id", "st.step_id")
     .leftJoin("ingredients as i", "st.ingredient_id", "i.ingredient_id")
-    .select("r.recipe_name", "r.created_at", "s.*", "st.quantity", "i.*");
+    .select("r.recipe_name", "r.created_at", "s.*", "st.quantity", "i.*")
+    .where("r.recipe_id", recipe_id)
+    .orderBy("s.step_number");
 
-  return rows;
+  const result = {
+    recipe_id: rows[0].recipe_id,
+    recipe_name: rows[0].recipe_name,
+    created_at: rows[0].created_at,
+    steps: [],
+  };
+
+  rows.forEach((row) => {
+    result.steps.push({
+      step_id: row.step_id,
+      step_number: row.step_number,
+      instructions: row.instructions,
+      ingredients: createIngredients(),
+    });
+  });
+
+  const createIngredients = () => {
+    return rows.forEach((row) => {
+      if (row.ingredient_id) {
+        result.steps.ingredients.push({
+          ingredient_id: row.steps.ingredient_id,
+          ingredient_name: row.steps.ingredient_name,
+          quantity: row.steps.quantity,
+        });
+      }
+    });
+  };
+
+  return result;
 }
 
 module.exports = {
